9843a89d80e878964a4ac16e95c38ea0ef7d1704
Guilherme <guilherme.ferreira28@outlook.com>
2019-07-27 12:03:34 +0200

    Merge master into gh-pages

---
+++ dist/views/pimba/pimba.js
@@ -0,0 +1,22 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Pimba = (function () {
+        function Pimba() {
+            this.songs = [{
+                    "name": "Raised in the projects",
+                    "band": "Project Pat",
+                    "link": "https://www.youtube.com/embed/UceaB4D0jpo"
+                },
+                {
+                    "name": "I'm On A Boat",
+                    "band": "The Lonely Island",
+                    "link": "https://www.youtube.com/embed/avaSdC0QOUM"
+                }];
+        }
+        return Pimba;
+    }());
+    exports.Pimba = Pimba;
+});
+
+//# sourceMappingURL=pimba.js.map
---
+++ dist/views/punk-rock/punk-rock.js
@@ -0,0 +1,22 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var PunkRock = (function () {
+        function PunkRock() {
+            this.songs = [{
+                    "name": "Raised in the projects",
+                    "band": "Project Pat",
+                    "link": "https://www.youtube.com/embed/UceaB4D0jpo"
+                },
+                {
+                    "name": "I'm On A Boat",
+                    "band": "The Lonely Island",
+                    "link": "https://www.youtube.com/embed/avaSdC0QOUM"
+                }];
+        }
+        return PunkRock;
+    }());
+    exports.PunkRock = PunkRock;
+});
+
+//# sourceMappingURL=punk-rock.js.map
