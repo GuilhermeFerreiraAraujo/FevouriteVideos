be1ff68c25049fed0f38c1931fd0eef771263c13
Guilherme <guilherme.ferreira28@outlook.com>
2019-07-27 11:59:39 +0200

    Add videos

---
+++ src/views/pimba/pimba.ts
@@ -0,0 +1,14 @@
+import { Song } from "src/models/shared.models";
+
+export class Pimba{
+  public songs: Song[] = [{
+    "name": "Raised in the projects",
+    "band": "Project Pat",
+    "link": "https://www.youtube.com/embed/UceaB4D0jpo"
+  },
+  {
+    "name": "I'm On A Boat",
+    "band": "The Lonely Island",
+    "link": "https://www.youtube.com/embed/avaSdC0QOUM"
+  }];
+}
